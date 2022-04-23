70300bddd5c9fd9823b9e2c7f503d22781899edc
ShivamShah410 <shivamshah.410@gmail.com>
2022-04-23 17:09:28 +0530

    Enhancing js file

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,111 +1,9 @@
 $(document).ready(function () {
-  var output = $("#tbody");
-  var input = $("#ipword");
-
-
   loadWords();
-
-  $("#addWord").on("click", function () {
-    if (input.val() !== "" && input.val() !== null) {
-      var ip = {
-        word: input.val(),
-      };
-
-      $.ajax({
-        type: "POST",
-        url: "/words/add",
-        data: JSON.stringify(ip),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        dataType: "json",
-        success: function (op) {
-          output.append(
-            "<tr>" +
-              "<td>" +
-              op.word +
-              "</td>" +
-              "<td>" +
-              "<button id=" +
-              op.id +
-              " class='edit'>Edit</button>" +
-              "</td>" +
-              "<td>" +
-              "<button id=" +
-              op.id +
-              " class='delete'>Delete</button>" +
-              "</td>" +
-              "</tr>"
-          );
-          input.val('');
-        },
-        error: function() {
-          alert("Error adding Word");
-        }
-      });
-    } else {
-      alert("please enter some value!!");
-    }
-  });
-
-
-  output.delegate('.delete', 'click', function () {
-    var delEle = $(this);
-    var id = $(this).attr("id");
-
-    $.ajax({
-      type: "DELETE",
-      url: "/words/delete/" + id,
-      success: function (op) {
-        delEle.parent().parent().remove();
-      },
-      error: function () {
-        alert("Error deleting Word");
-      },  
-    });
-  });
-
-  output.delegate(".edit", "click", function () {
-    var editEle = $(this);
-    var prevWord = editEle.parent().siblings()[0].innerText;
-    var id = $(this).attr("id");
-    var res = prompt("Enter the new word:", prevWord);
-
-    if(res) {
-
-      var ipdata = {
-        "id": id,
-        "word": res
-      }
-
-      $.ajax({
-        type: "PUT",
-        url: "/words/update/",
-        data: JSON.stringify(ipdata),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        dataType: "json",
-        success: function (op) {
-          editEle.parent().siblings()[0].innerText = res;
-          // loadWords();
-        },
-        error: function () {
-          alert("Error editing Word");
-        },
-      });
-    }
-  });
-
 });
 
-
 function loadWords() {
-
   var output = $("#tbody");
-
   output.empty();
   $.ajax({
     type: "GET",
@@ -134,3 +32,98 @@ function loadWords() {
     },
   });
 }
+
+$(".delete").on("click", function () {
+  if (confirm("Do you really want to delete this word?")) {
+    var delEle = $(this);
+    var id = $(this).attr("id");
+
+    $.ajax({
+      type: "DELETE",
+      url: "/words/delete/" + id,
+      success: function (op) {
+        delEle.parent().parent().remove();
+      },
+      error: function () {
+        alert("Error deleting Word");
+      },
+    });
+  }
+});
+
+$(".edit").on("click", function () {
+  var editEle = $(this);
+  var prevWord = editEle.parent().siblings()[0].innerText;
+  var id = $(this).attr("id");
+  var res = prompt("Enter the new word:", prevWord);
+
+  if (res) {
+    var ipdata = {
+      id: id,
+      word: res,
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: "/words/update/",
+      data: JSON.stringify(ipdata),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      dataType: "json",
+      success: function (op) {
+        editEle.parent().siblings()[0].innerText = res;
+        // loadWords();
+      },
+      error: function () {
+        alert("Error editing Word");
+      },
+    });
+  }
+});
+
+$("#addWord").on("click", function () {
+  var input = $("#ipword");
+  if (input.val() !== "" && input.val() !== null) {
+    var ip = {
+      word: input.val(),
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "/words/add",
+      data: JSON.stringify(ip),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      dataType: "json",
+      success: function (op) {
+        output.append(
+          "<tr>" +
+            "<td>" +
+            op.word +
+            "</td>" +
+            "<td>" +
+            "<button id=" +
+            op.id +
+            " class='edit'>Edit</button>" +
+            "</td>" +
+            "<td>" +
+            "<button id=" +
+            op.id +
+            " class='delete'>Delete</button>" +
+            "</td>" +
+            "</tr>"
+        );
+        input.val("");
+      },
+      error: function () {
+        alert("Error adding Word");
+      },
+    });
+  } else {
+    alert("please enter some value!!");
+  }
+});
